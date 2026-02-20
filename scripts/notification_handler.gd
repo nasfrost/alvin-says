55899c669a17e4fb9cfb019600f43951ac18ab94
nasfrost <nasrahman@protonmail.com>
2026-02-20 22:28:29 +0000

    bugfix

--- scripts/notification_handler.gd
+++ scripts/notification_handler.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _notification(what):
 	if what == NOTIFICATION_WM_GO_BACK_REQUEST:
-		if get_tree().current_scene.name == "Home":
+		if get_tree().current_scene.name == "home":
 			get_tree().quit()
 		else:
 			get_tree().change_scene_to_file("res://scenes/home.tscn")
